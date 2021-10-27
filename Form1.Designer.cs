1a3afed950c68bf755c86f19472c367d2598e1e3
kraken276 <Nikro@bk.ru>
2021-10-27 15:09:46 +0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿
+namespace TestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ChooseFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BusCountLabel = new System.Windows.Forms.Label();
+            this.StationsCountLabel = new System.Windows.Forms.Label();
+            this.Panel = new System.Windows.Forms.Panel();
+            this.StartStationTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EndStationTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.StartTimeTextBox = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ResultPanel = new System.Windows.Forms.Panel();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.ResultPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ChooseFileMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ChooseFileMenuItem
+            // 
+            this.ChooseFileMenuItem.Name = "ChooseFileMenuItem";
+            this.ChooseFileMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.ChooseFileMenuItem.Text = "Выбрать файл";
+            this.ChooseFileMenuItem.Click += new System.EventHandler(this.ChooseFile);
+            // 
+            // BusCountLabel
+            // 
+            this.BusCountLabel.AutoSize = true;
+            this.BusCountLabel.Location = new System.Drawing.Point(12, 60);
+            this.BusCountLabel.Name = "BusCountLabel";
+            this.BusCountLabel.Size = new System.Drawing.Size(137, 15);
+            this.BusCountLabel.TabIndex = 1;
+            this.BusCountLabel.Text = "Количество автобусов: ";
+            // 
+            // StationsCountLabel
+            // 
+            this.StationsCountLabel.AutoSize = true;
+            this.StationsCountLabel.Location = new System.Drawing.Point(12, 84);
+            this.StationsCountLabel.Name = "StationsCountLabel";
+            this.StationsCountLabel.Size = new System.Drawing.Size(138, 15);
+            this.StationsCountLabel.TabIndex = 2;
+            this.StationsCountLabel.Text = "Количество остановок: ";
+            // 
+            // Panel
+            // 
+            this.Panel.AutoScroll = true;
+            this.Panel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Panel.Location = new System.Drawing.Point(12, 113);
+            this.Panel.Name = "Panel";
+            this.Panel.Size = new System.Drawing.Size(600, 100);
+            this.Panel.TabIndex = 3;
+            // 
+            // StartStationTextBox
+            // 
+            this.StartStationTextBox.Location = new System.Drawing.Point(19, 257);
+            this.StartStationTextBox.Name = "StartStationTextBox";
+            this.StartStationTextBox.Size = new System.Drawing.Size(86, 23);
+            this.StartStationTextBox.TabIndex = 4;
+            this.StartStationTextBox.TextChanged += new System.EventHandler(this.StationChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 239);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Начальная остановка";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 283);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Конечная остановка";
+            // 
+            // EndStationTextBox
+            // 
+            this.EndStationTextBox.Location = new System.Drawing.Point(19, 301);
+            this.EndStationTextBox.Name = "EndStationTextBox";
+            this.EndStationTextBox.Size = new System.Drawing.Size(86, 23);
+            this.EndStationTextBox.TabIndex = 7;
+            this.EndStationTextBox.TextChanged += new System.EventHandler(this.StationChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Время отправления";
+            // 
+            // StartTimeTextBox
+            // 
+            this.StartTimeTextBox.Location = new System.Drawing.Point(214, 257);
+            this.StartTimeTextBox.Name = "StartTimeTextBox";
+            this.StartTimeTextBox.Size = new System.Drawing.Size(88, 23);
+            this.StartTimeTextBox.TabIndex = 9;
+            this.StartTimeTextBox.TextChanged += new System.EventHandler(this.StartTimeChanged);
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(7, 13);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(86, 28);
+            this.CalculateButton.TabIndex = 10;
+            this.CalculateButton.Text = "Рассчитать";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.Calculate);
+            // 
+            // ResultPanel
+            // 
+            this.ResultPanel.Controls.Add(this.ResultLabel);
+            this.ResultPanel.Controls.Add(this.CalculateButton);
+            this.ResultPanel.Location = new System.Drawing.Point(12, 347);
+            this.ResultPanel.Name = "ResultPanel";
+            this.ResultPanel.Size = new System.Drawing.Size(600, 121);
+            this.ResultPanel.TabIndex = 11;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(7, 48);
+            this.ResultLabel.MaximumSize = new System.Drawing.Size(550, 1000);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 15);
+            this.ResultLabel.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 499);
+            this.Controls.Add(this.ResultPanel);
+            this.Controls.Add(this.StartTimeTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EndStationTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartStationTextBox);
+            this.Controls.Add(this.Panel);
+            this.Controls.Add(this.StationsCountLabel);
+            this.Controls.Add(this.BusCountLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResultPanel.ResumeLayout(false);
+            this.ResultPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ChooseFileMenuItem;
+        private System.Windows.Forms.Label BusCountLabel;
+        private System.Windows.Forms.Label StationsCountLabel;
+        private System.Windows.Forms.Panel Panel;
+        private System.Windows.Forms.TextBox StartStationTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EndStationTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox StartTimeTextBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Panel ResultPanel;
+        private System.Windows.Forms.Label ResultLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        private List<Arc> Arcs { get; set; }
+        private List<Bus> Buses { get; set; }
+        private readonly int finalTime = 1440;
+        private int startStation = -1;
+        private int endStation = -1;
+        private int startTime = -1;
+        private int stationsCount;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChooseFile(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.RestoreDirectory = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileName = dialog.FileName;
+                using (var fileStream = new FileStream(fileName, FileMode.Open))
+                {
+                    ProcessFile(fileStream);
+                    UpdateForm();
+                }
+            }
+        }
+
+        private void UpdateForm()
+        {
+            BusCountLabel.Text = "Количество автобусов: " + Buses.Count;
+            StationsCountLabel.Text = "Количество остановок: " + stationsCount;
+            ResultLabel.Text = "";
+            for (int i = 0; i < Buses.Count; i++)
+            {
+                var bus = Buses[i];
+                Panel.Controls.Add(new Label()
+                {
+                    Text = $"Автобус {i + 1}: стоимость проезда {bus.Fare} рублей, время отправления {TranslateTime(bus.StartTime)}, полное время маршрута {bus.TotalTime} минут",
+                    Location = new System.Drawing.Point(10, i * 20 + 10),
+                    AutoSize = true
+                });
+            }
+        }
+
+        private void ProcessFile(FileStream fileStream)
+        {
+            Buses = new List<Bus>();
+            Arcs = new List<Arc>();
+            using (var reader = new StreamReader(fileStream))
+            {
+                var busesCount = int.Parse(reader.ReadLine().Trim());
+                stationsCount = int.Parse(reader.ReadLine().Trim());
+                var times = reader.ReadLine().Trim().Split(' ');
+                var fares = reader.ReadLine().Trim().Split(' ');
+                Bus bus;
+                for (int i = 0; i < times.Length; i++)
+                {
+                    bus = new Bus();
+                    bus.Number = i + 1;
+                    bus.StartTime = TranslateTime(times[i]);
+                    bus.Fare = int.Parse(fares[i]);
+                    var routeString = reader.ReadLine().Trim();
+                    ExtractArcs(routeString, bus);
+                    Buses.Add(bus);
+                }
+            }
+        }
+
+        private void ExtractArcs(string routeString, Bus bus)
+        {
+            int totalTime = 0;
+            bus.Arcs = new List<Arc>();
+            var parts = routeString.Split(' ');
+            var length = (parts.Length - 1) / 2;
+            if (length < 1)
+            {
+                bus.TotalTime = 0;
+                return;
+            }
+            Arc arc;
+            for (int i = 0; i < length - 1; i++)
+            {
+                arc = new Arc();
+                arc.StartStation = int.Parse(parts[i + 1]) - 1;
+                arc.EndStation = int.Parse(parts[i + 2]) - 1;
+                arc.Time = int.Parse(parts[i + length + 1]);
+                arc.Bus = bus;
+                totalTime += arc.Time;
+                Arcs.Add(arc);
+                bus.Arcs.Add(arc);
+            }
+            arc = new Arc();
+            arc.StartStation = int.Parse(parts[length]) - 1;
+            arc.EndStation = int.Parse(parts[1]) - 1;
+            arc.Time = int.Parse(parts[parts.Length - 1]);
+            arc.Bus = bus;
+            totalTime += arc.Time;
+            Arcs.Add(arc);
+            bus.Arcs.Add(arc);
+            bus.TotalTime = totalTime;
+        }
+
+        private int TranslateTime(string time)
+        {
+            var parts = time.Split(':');
+            return int.Parse(parts[0]) * 60 + int.Parse(parts[1]);
+        }
+
+        private string TranslateTime(int time)
+        {
+            return (time / 60).ToString() + ":" + (time % 60).ToString("00");
+        }
+
+        private void StationChanged(object sender, EventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            int value;
+            if (!int.TryParse(textBox.Text, out value))
+            {
+                value = -1;
+            }
+            if (textBox == StartStationTextBox)
+                startStation = value - 1;
+            else
+                endStation = value - 1;
+        }
+
+        private void StartTimeChanged(object sender, EventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            if (Regex.Match(textBox.Text, @"^(\d|\d\d):\d\d$").Success)
+            {
+                startTime = TranslateTime(textBox.Text);
+            }
+            else
+            {
+                startTime = -1;
+            }
+        }
+
+        private void Calculate(object sender, EventArgs e)
+        {
+            string validateString = ValidateInputs();
+            if (validateString == "")
+            {
+                var fastestWay = CalculateWay(WayType.Fast, (Arc nextArc, Bus lastBus, int value) =>
+                {
+                    return nextArc.Time + GetWaitTime(nextArc, value);
+                });
+                var cheapestWay = CalculateWay(WayType.Cheap, (Arc nextArc, Bus lastBus, int value) =>
+                {
+                    if (lastBus != nextArc.Bus)
+                        return nextArc.Bus.Fare;
+                    else
+                        return 0;
+                });
+                ResultLabel.Text = GetWayDescription(fastestWay, WayType.Fast) + ";\n";
+                ResultLabel.Text += GetWayDescription(cheapestWay, WayType.Cheap);
+            }
+            else
+                ResultLabel.Text = validateString;
+        }
+
+        private string GetWayDescription(Way way, WayType type)
+        {
+            string stringType = type == WayType.Fast ? "быстрый" : "дешевый";
+            if (startTime + way.Time > finalTime)
+                return $"Нельзая построить {stringType} маршрут: слишком поздно";
+            else
+                return $"Самый {stringType} маршрут стоит {way.TotalFare} рублей и занимает {way.Time} минут. Описание: " +
+                    string.Join(", затем ", way.Arcs.Select(x => { return $"на станции {x.StartStation + 1} сесть на {x.Bus.Number} автобус до станции {x.EndStation + 1}"; }));
+        }
+
+        private string ValidateInputs()
+        {
+            List<string> validateParts = new List<string>();
+            if (startStation < 0 || startStation > stationsCount - 1)
+                validateParts.Add("неправильно указана первая остановка");
+            if (endStation < 0 || endStation > stationsCount - 1)
+                validateParts.Add("неправильно указана конечная остановка");
+            if (startTime < 0 || startTime > finalTime)
+                validateParts.Add("неправильно указано время отправки");
+            var validateString = string.Join("; ", validateParts);
+            if (validateParts.Count > 0)
+                return validateString[0].ToString().ToUpper() + validateString.Substring(1);
+            else
+                return "";
+        }
+
+        private Way CalculateWay(WayType type, Func<Arc, Bus, int, int> func)
+        {
+            int[] time = new int[stationsCount];
+            int[] fare = new int[stationsCount];
+            var ways = new List<Arc>[stationsCount];
+            int length = time.Length;
+            for (int i = 0; i < length; i++)
+            {
+                time[i] = int.MaxValue;
+                fare[i] = int.MaxValue;
+            }
+            time[startStation] = 0;
+            fare[startStation] = 0;
+            ways[startStation] = new List<Arc>();
+            int weight;
+            Bus lastBus = null;
+            if (type == WayType.Fast)
+            {
+                for (int i = 0; i < length - 1; i++)
+                {
+                    foreach (var arc in Arcs)
+                    {
+                        if (ways[arc.StartStation] != null && ways[arc.StartStation].Count > 0)
+                            lastBus = ways[arc.StartStation].Last().Bus;
+                        else
+                            lastBus = null;
+                        weight = arc.Time + GetWaitTime(arc, time[arc.StartStation]);
+                        if (time[arc.StartStation] != int.MaxValue && time[arc.EndStation] > time[arc.StartStation] + weight)
+                        {
+                            time[arc.EndStation] = time[arc.StartStation] + weight;
+                            ways[arc.EndStation] = GetWay(ways[arc.StartStation], arc);
+                            if (lastBus != arc.Bus)
+                                fare[arc.EndStation] = fare[arc.StartStation] + arc.Bus.Fare;
+                            else
+                                fare[arc.EndStation] = fare[arc.StartStation];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < length - 1; i++)
+                {
+                    foreach (var arc in Arcs)
+                    {
+                        if (ways[arc.StartStation] != null && ways[arc.StartStation].Count > 0)
+                            lastBus = ways[arc.StartStation].Last().Bus;
+                        else
+                            lastBus = null;
+                        if (lastBus != arc.Bus)
+                            weight = arc.Bus.Fare;
+                        else
+                            weight = 0;
+                        if (fare[arc.StartStation] != int.MaxValue && fare[arc.EndStation] > fare[arc.StartStation] + weight)
+                        {
+                            fare[arc.EndStation] = fare[arc.StartStation] + weight;
+                            ways[arc.EndStation] = GetWay(ways[arc.StartStation], arc);
+                            time[arc.EndStation] = time[arc.StartStation] + arc.Time + GetWaitTime(arc, time[arc.StartStation]);
+                        }
+                    }
+                }
+            }
+            return new Way
+            {
+                Arcs = GetFixedRoute(ways[endStation]),
+                Time = time[endStation],
+                TotalFare = fare[endStation]
+            };
+        }
+
+        private List<Arc> GetFixedRoute(List<Arc> arcs)
+        {
+            var fixedRoute = new List<Arc>();
+            var length = arcs.Count;
+            Bus bus = null;
+            Arc arc = null;
+            for (int i = 0; i < length; i++)
+            {
+                if (arcs[i].Bus != bus)
+                {
+                    arc = new Arc()
+                    {
+                        StartStation = arcs[i].StartStation,
+                        EndStation = arcs[i].EndStation,
+                        Bus = arcs[i].Bus
+                    };
+                    fixedRoute.Add(arc);
+                    bus = arcs[i].Bus;
+                }
+                else
+                {
+                    arc.EndStation = arcs[i].EndStation;
+                }
+            }
+            return fixedRoute;
+        }
+
+        private int GetWaitTime(Arc arc, int passedTime)
+        {
+            int waitTime = arc.Bus.Arcs.TakeWhile(x => x.StartStation != arc.StartStation).Sum(x => x.Time);
+            waitTime = startTime + passedTime - arc.Bus.StartTime - waitTime;
+            waitTime = waitTime % arc.Bus.TotalTime;
+            if (waitTime < 0)
+                waitTime += arc.Bus.TotalTime;
+            return waitTime;
+        }
+
+        private List<Arc> GetWay(List<Arc> sourceWay, Arc arc)
+        {
+            var way = new List<Arc>();
+            foreach (var a in sourceWay)
+                way.Add(a);
+            way.Add(arc);
+            return way;
+        }
+    }
+
+    public class Way
+    {
+        public List<Arc> Arcs { get; set; }
+        public int Time { get; set; }
+        public int TotalFare { get; set;}
+    }
+
+    public class Bus
+    {
+        public int Number { get; set; }
+        public int StartTime { get; set; }
+        public int TotalTime { get; set; }
+        public int Fare { get; set; }
+        public List<Arc> Arcs { get; set; }
+    }
+
+    public class Arc
+    {
+        public int StartStation { get; set; }
+        public int EndStation { get; set; }
+        public int Time { get; set; }
+        public Bus Bus { get; set; }
+    }
+
+    public enum WayType
+    {
+        Fast = 1,
+        Cheap = 2
+    }
+}
